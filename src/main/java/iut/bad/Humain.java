494ac4522c8e4adccf2881ca8dff9cd343bd9a50
Mar-Tini <mart00tini@gmail.com>
2026-05-04 23:26:12 +0300

    feat :  methodes manger() et boire() dans Humain"

--- src/main/java/iut/bad/Humain.java
+++ src/main/java/iut/bad/Humain.java
@@ -24,4 +24,12 @@ public class Humain {
     public void details() {
         System.out.println(toString());
     }
+
+    public void manger() {
+        System.out.println(prenom + " est en train de manger.");
+    }
+
+    public void boire() {
+        System.out.println(prenom + " est en train de boire.");
+    }
 }
