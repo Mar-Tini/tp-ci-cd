1b376969d1b4313990691dc1f18cfaf526072994
Mar-Tini <mart00tini@gmail.com>
2026-05-04 23:07:15 +0300

    fea : ajout classe Homme

--- src/main/java/iut/bad/Homme.java
+++ src/main/java/iut/bad/Homme.java
@@ -2,5 +2,18 @@ package iut.bad;
 
 
 public class Homme {
+    private String nom;
+    private String prenom;
+    private int age;
 
+    public Homme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    
+    // Getters
+    public String getNom() { return nom; }
+    public String getPrenom() { return prenom; }
+    public int getAge() { return age; }
 }
