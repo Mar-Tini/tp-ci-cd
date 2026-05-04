d6619c9e11aa3e0742e6e99fb426817f82337638
Mar-Tini <mart00tini@gmail.com>
2026-05-04 23:09:16 +0300

    feat : ajout classe Femme

--- src/main/java/iut/bad/Femme.java
+++ src/main/java/iut/bad/Femme.java
@@ -1,5 +1,19 @@
 package iut.bad;
 
+
 public class Femme {
-    
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Femme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    // Getters
+    public String getNom() { return nom; }
+    public String getPrenom() { return prenom; }
+    public int getAge() { return age; }
 }
