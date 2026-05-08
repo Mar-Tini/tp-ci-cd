3a2b9723b8fe52ea09e36cff1b2174e206be9588
Mar-Tini <mart00tini@gmail.com>
2026-05-08 09:14:11 +0300

    test : add testSimpleAdd

--- src/test/java/fr/emse/test/MoneyTest.java
+++ src/test/java/fr/emse/test/MoneyTest.java
@@ -1,5 +1,16 @@
 package fr.emse.test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class MoneyTest {
     
+    @Test
+    public void testSimpleAdd() {
+        Money m12CHF = new Money(12, "CHF");
+        Money m14CHF = new Money(14, "CHF");
+        Money expected = new Money(26, "CHF");
+        Money result   = m12CHF.add(m14CHF);
+        assertTrue(expected.equals(result));  
+    }
+
 }
