a04188e18eb5a097432a0906d26d23d13a3ed3ec
Mar-Tini <mart00tini@gmail.com>
2026-05-08 09:53:09 +0300

    feat: MoneyBag implémente IMoney

--- src/main/java/fr/emse/test/MoneyBag.java
+++ src/main/java/fr/emse/test/MoneyBag.java
@@ -2,40 +2,59 @@ package fr.emse.test;
 
 import java.util.Vector;
 
-public class MoneyBag {
+public class MoneyBag implements IMoney {
+
     private Vector<Money> fMonies = new Vector<>();
 
-    MoneyBag(Money m1, Money m2) {
+    public MoneyBag(Money m1, Money m2) {
         appendMoney(m1);
         appendMoney(m2);
     }
 
-    MoneyBag(Money[] bag) {
-        for (Money m : bag) appendMoney(m);
+    public MoneyBag(Money[] bag) {
+        for (Money m : bag) {
+            appendMoney(m);
+        }
     }
 
     private void appendMoney(Money m) {
+
         if (fMonies.isEmpty()) {
             fMonies.add(m);
+            return;
+        }
+
+        int i = 0;
+
+        while (i < fMonies.size()
+                && !fMonies.get(i).currency().equals(m.currency())) {
+            i++;
+        }
+
+        if (i >= fMonies.size()) {
+
+            fMonies.add(m);
+
         } else {
-            int i = 0;
-            while (i < fMonies.size() &&
-                   !fMonies.get(i).currency().equals(m.currency()))
-                i++;
-            if (i >= fMonies.size()) {
-                fMonies.add(m);
-            } else {
-                fMonies.set(i, new Money(
-                    fMonies.get(i).amount() + m.amount(),
-                    m.currency()));
-            }
+
+            Money existing = fMonies.get(i);
+
+            fMonies.set(i,
+                    new Money(
+                            existing.amount() + m.amount(),
+                            m.currency()));
         }
     }
 
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof MoneyBag)) return false;
+
+        if (!(o instanceof MoneyBag)) {
+            return false;
+        }
+
         MoneyBag other = (MoneyBag) o;
+
         return fMonies.equals(other.fMonies);
     }
 
@@ -44,10 +63,35 @@ public class MoneyBag {
         return fMonies.hashCode();
     }
 
-    public IMoney addMoney(Money money) {
-        Money[] newMonies = new Money[fMonies.size()];
-        for (int i = 0; i < fMonies.size(); i++)
-            newMonies[i] = fMonies.get(i);
-        return new MoneyBag(newMonies).addMoney(money);
+    @Override
+    public IMoney add(IMoney m) {
+        return m.addMoneyBag(this);
+    }
+
+    @Override
+    public IMoney addMoney(Money m) {
+
+        Money[] bag = new Money[fMonies.size() + 1];
+
+        for (int i = 0; i < fMonies.size(); i++) {
+            bag[i] = fMonies.get(i);
+        }
+
+        bag[fMonies.size()] = m;
+
+        return new MoneyBag(bag);
+    }
+
+    @Override
+    public IMoney addMoneyBag(MoneyBag mb) {
+
+        Vector<Money> all = new Vector<>();
+
+        all.addAll(fMonies);
+        all.addAll(mb.fMonies);
+
+        Money[] bag = all.toArray(new Money[0]);
+
+        return new MoneyBag(bag);
     }
 }
